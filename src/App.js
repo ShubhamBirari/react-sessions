b51ada90fb3811859ce11a26fc0e92f616c2d14c
ShubhamBirari <shubham1851998@gmail.com>
2024-03-16 11:48:52 +0530

    chore: ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import Header from "./layout/Header";
 
+const bookList = [
+  {
+    id: "abc",
+    name: "Book 1",
+    quantity: 0,
+    price: 50,
+  },
+  {
+    id: "abc1",
+    name: "Book 2",
+    quantity: 0,
+    price: 50,
+  },
+  {
+    id: "abc2",
+    name: "Book 3",
+    quantity: 0,
+    price: 50,
+  },
+  {
+    id: "abc3",
+    name: "Book 4",
+    quantity: 0,
+    price: 50,
+  },
+  {
+    id: "abc4",
+    name: "Book 5",
+    quantity: 0,
+    price: 50,
+  },
+  {
+    id: "abc5",
+    name: "Book 6",
+    quantity: 0,
+    price: 50,
+  },
+];
+
 export default function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList(bookList);
+  }, []);
+
   return (
     <>
       <Header />
 
       <div className="book-list">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {list.map((book) => (
+          <Card key={book.id} book={book} />
+        ))}
       </div>
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import Button from "./Button";
 
-const Card = () => {
+const Card = ({ book }) => {
+  const [quantity, setQuantity] = useState(book.quantity);
+
   return (
     <div className="card-container">
       <div className="card-image">Book Image</div>
+      <div>{book.name}</div>
+      <div>{book.price} </div>
       <div className="card-footer">
-        <Button style={{ background: "red" }}>-</Button>
-        <Button style={{ background: "green" }}>+</Button>
+        <Button
+          style={{ background: "red" }}
+          onClick={() => setQuantity((book) => book - 1)}
+        >
+          -
+        </Button>
+        {quantity}
+        <Button
+          style={{ background: "green" }}
+          onClick={() => setQuantity((book) => book + 1)}
+        >
+          +
+        </Button>
       </div>
     </div>
   );
